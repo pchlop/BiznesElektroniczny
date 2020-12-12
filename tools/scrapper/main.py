47c8932a5934fd0a0ecfadacf486c6f65befb1b0
Tymoteusz Malinowski <ecyloel@gmail.com>
2020-12-12 12:28:32 +0100

    Set image location for prestashop

--- tools/scrapper/main.py
+++ tools/scrapper/main.py
@@ -11,26 +11,27 @@ import sys
 # Configuration
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-COURSE_LIMIT = 100
+COURSE_LIMIT = 40
 OUTPUT_FILE = 'courses.csv'
 OUTPUT_IMAGE_DIR = 'images/'
-LANGUAGE = 'pl'
+LANGUAGE = 'en'
 
 client = UdemyClient(CLIENT_ID, CLIENT_SECRET)
 images = {}
 total = math.ceil(COURSE_LIMIT / 20)
-total_items = total * 20
 
 # Download courses data
-with tqdm(total=total_items, desc='Downloading courses data', file=sys.stdout) as pbar:
+with tqdm(total=COURSE_LIMIT, desc='Downloading courses data', file=sys.stdout) as pbar:
     for i in range(total):
         courses = client.courses(page=i, page_size=1, language=LANGUAGE)  # 20 results per request
         for item in courses['results']:
             images[item['published_title']] = item['image_480x270']
+            image_location = '/'.join([str(x) for x in str(item['id'])]) + '/' + item['published_title'] + '.jpg'
             with open(OUTPUT_FILE, mode='a') as csv_file:
                 courses_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                 courses_writer.writerow([
-                    item['title'], item['published_title'], item['headline'], item['price'], item['image_480x270']
+                    item['id'], item['title'], item['published_title'], item['headline'],
+                    item['price_detail']['amount'], image_location
                 ])
         pbar.update(20)
 
@@ -38,7 +39,7 @@ with tqdm(total=total_items, desc='Downloading courses data', file=sys.stdout) a
 Path('./images').mkdir(parents=True, exist_ok=True)
 
 # Download courses images and save them to images catalog
-with tqdm(total=total_items, desc='Downloading courses images', file=sys.stdout) as pbar:
+with tqdm(total=len(images), desc='Downloading courses images', file=sys.stdout) as pbar:
     for filename, url in images.items():
         urllib.request.urlretrieve(url, OUTPUT_IMAGE_DIR + filename + '.jpg')
         pbar.update(1)
