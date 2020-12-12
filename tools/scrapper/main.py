cee48c8585a8b071ef6b318b9bcd67221a7d4359
Tymoteusz Malinowski <ecyloel@gmail.com>
2020-12-12 03:43:18 +0100

    Add Udemy scrapper

---
+++ tools/scrapper/main.py
@@ -0,0 +1,29 @@
+from config import settings
+from client.UdemyClient import UdemyClient
+import urllib.request
+import os
+import csv
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+OUTPUT_FILE = 'courses.csv'
+OUTPUT_IMAGE_DIR = 'images/'
+
+client = UdemyClient(CLIENT_ID, CLIENT_SECRET)
+images = {}
+
+print("Downloading courses data...")
+for i in range(1):
+    courses = client.courses(page=i, page_size=1)  # 20 results per request
+    for item in courses['results']:
+        with open(OUTPUT_FILE, mode='a') as csv_file:
+            images[item['published_title']] = item['image_480x270']
+            courses_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            courses_writer.writerow([
+                item['title'], item['published_title'], item['headline'], item['price'], item['image_480x270']
+            ])
+
+print("Downloading courses images...")
+for filename, url in images.items():
+    urllib.request.urlretrieve(url, OUTPUT_IMAGE_DIR + filename + '.jpg')
+
