2e030d09c42a5bc59398e87e5643855c74e27e26
Tymoteusz Malinowski <ecyloel@gmail.com>
2020-12-12 11:58:14 +0100

    Add language option, total items to download

--- tools/scrapper/main.py
+++ tools/scrapper/main.py
@@ -8,20 +8,23 @@ import csv
 import math
 import sys
 
+# Configuration
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-COURSE_LIMIT = 40
+COURSE_LIMIT = 100
 OUTPUT_FILE = 'courses.csv'
 OUTPUT_IMAGE_DIR = 'images/'
+LANGUAGE = 'pl'
 
 client = UdemyClient(CLIENT_ID, CLIENT_SECRET)
 images = {}
 total = math.ceil(COURSE_LIMIT / 20)
+total_items = total * 20
 
 # Download courses data
-with tqdm(total=total * 20, desc='Downloading courses data', file=sys.stdout) as pbar:
+with tqdm(total=total_items, desc='Downloading courses data', file=sys.stdout) as pbar:
     for i in range(total):
-        courses = client.courses(page=i, page_size=1)  # 20 results per request
+        courses = client.courses(page=i, page_size=1, language=LANGUAGE)  # 20 results per request
         for item in courses['results']:
             images[item['published_title']] = item['image_480x270']
             with open(OUTPUT_FILE, mode='a') as csv_file:
@@ -35,7 +38,7 @@ with tqdm(total=total * 20, desc='Downloading courses data', file=sys.stdout) as
 Path('./images').mkdir(parents=True, exist_ok=True)
 
 # Download courses images and save them to images catalog
-with tqdm(total=len(images), desc='Downloading courses images', file=sys.stdout) as pbar:
+with tqdm(total=total_items, desc='Downloading courses images', file=sys.stdout) as pbar:
     for filename, url in images.items():
         urllib.request.urlretrieve(url, OUTPUT_IMAGE_DIR + filename + '.jpg')
         pbar.update(1)
