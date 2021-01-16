8f51b286b55fb8feebd2d796c1031abd168f0c90
Tymoteusz Malinowski <ecyloel@gmail.com>
2021-01-16 17:09:30 +0100

    add docker compose

--- prestashop/app/config/parameters.php
+++ prestashop/app/config/parameters.php
@@ -1,11 +1,11 @@
 <?php return array (
   'parameters' => 
   array (
-    'database_host' => '127.0.0.1',
-    'database_port' => '',
+    'database_host' => 'biznes-elektroniczny-db',
+    'database_port' => '3306',
     'database_name' => 'prestashop',
     'database_user' => 'root',
-    'database_password' => 'Biznes2020',
+    'database_password' => '12341234',
     'database_prefix' => 'ps_',
     'database_engine' => 'InnoDB',
     'mailer_transport' => 'smtp',
